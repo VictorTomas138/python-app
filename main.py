9fc0f2bedd485e3a1e2cd6d7860f0306cc5193e7
Nicolas Victor Tomas Choque <103605598+VictorTomas138@users.noreply.github.com>
2023-10-10 22:23:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import streamlit as st
+import io
+
+
+uploaded_files = st.file_uploader(
+    "Escoge archivos",
+    accept_multiple_files=True)
+
+if len(uploaded_files) > 0:
+    try:
+        images = []
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            img = Image.open(
+                io.BytesIO(bytes_data))
+            img = img.convert('RGB')
+            images.append(img)
+
+        file_name = st.text_input(
+            "Ingrese el nomnre del archivo:", "Archivo.pdf")
+
+        images[0].save(
+            file_name,
+            save_all=True,
+            append_images=images[1:]
+        )
+
+        with open(file_name, 'rb') as archivo:
+            st.download_button(
+                label='Descargar',
+                file_name=file_name,
+                data=archivo)
+    except Exception as e:
+        st.title('Error')
